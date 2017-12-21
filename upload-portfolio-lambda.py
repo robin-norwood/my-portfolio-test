fd6b2037d56b7d0ff20dee801cade34cbdc517a9
Robin Norwood <robin@acloud.guru>
2017-12-21 11:55:16 -0600

    Use bucket name and object key from codepipeline, and notify when codepipeline job is done

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -8,13 +8,27 @@ def lambda_handler(event, context):
     topic = sns.Topic('arn:aws:sns:us-east-1:766778111118:deployPortfolioTopic')
 
     try:
+        job = event.get("CodePipeline.job")
+
+        location = {
+            'bucketName': 'portfoliobuild.testrobin.net',
+            'objectKey': 'portfoliobuild.zip'
+        }
+
+        if job:
+            for artifact in job['data']['inputArtifacts']:
+                if artifact['name'] == 'MyAppBuild':
+                    location = artifact['location']['s3Location']
+
+        print "Building portfolio from " + str(location)
+
         s3 = boto3.resource('s3')
 
         portfolio_bucket = s3.Bucket('portfolio.testrobin.net')
-        build_bucket = s3.Bucket('portfoliobuild.testrobin.net')
+        build_bucket = s3.Bucket(location['bucketName'])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location['objectKey'], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -22,13 +36,20 @@ def lambda_handler(event, context):
                 portfolio_bucket.upload_fileobj(obj, nm,
                   ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+
+        print "Job done!"
+        topic.publish(Subject="Portfolio Deployed",
+          Message="Portfolio deployed successfully!")
+
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job['id'])
+
     except:
         topic.publish(Subject="Portfolio Deploy Failed",
           Message="Portfolio was not deployed successfully.")
         raise
 
-    print "Job done!"
-    topic.publish(Subject="Portfolio Deployed",
-      Message="Portfolio deployed successfully!")
 
     return 'Hello from Lambda'
